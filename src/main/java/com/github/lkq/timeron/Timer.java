fd9a2ff7eaff933e107321215b505f03f7a7b5e3
kliu <kingson708@gmail.com>
2018-03-26 09:29:30 +0800

    allow proxy on specific interface

--- src/main/java/com/github/lkq/timeron/Timer.java
+++ src/main/java/com/github/lkq/timeron/Timer.java
@@ -10,8 +10,29 @@ public class Timer {
         proxyFactory = new TimerProxyFactory(new AnnotationFinder());
     }
 
+    /**
+     * return a proxy object for target, Dynamic Proxy or CGLib proxy will be created
+     * depending on where the @Timer tag appears in the class hierarchy.
+     * If the @Timer only tagged on parent interface methods declaration, Dynamic Proxy will be created
+     * If the @Timer directly tagged on any class/superclass method in the hierarchy, CGLib proxy will be created
+     * @param target
+     * @param <T>
+     * @return
+     */
     public <T> T on(T target) {
         TimerProxy<T> proxy = proxyFactory.createProxy(target);
         return proxy.getProxy();
+    }
+
+    /**
+     * return a Dynamic Proxy for target, only the method declared in iface will be intercepted
+     * @param target
+     * @param iface
+     * @param <I>
+     * @param <C>
+     * @return
+     */
+    public <I, C extends I> I on(C target, Class<I> iface) {
+        throw new TimerException("Pending Implementation");
     }
 }
