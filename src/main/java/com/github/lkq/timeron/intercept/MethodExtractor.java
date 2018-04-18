5824585435faf3e3d0114b413aa232ab7d99c402
kliu <kingson708@gmail.com>
2018-04-18 22:04:39 +0800

    remove unused methods

--- src/main/java/com/github/lkq/timeron/intercept/MethodExtractor.java
+++ src/main/java/com/github/lkq/timeron/intercept/MethodExtractor.java
@@ -1,26 +1,11 @@
 package com.github.lkq.timeron.intercept;
 
-import com.github.lkq.timeron.ErrorReporter;
-import com.github.lkq.timeron.TimerException;
-
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MethodExtractor {
-
-    public List<Method> interceptedMethods(Class clz, Map<Class, List<Method>> interceptedMethods) {
-        if (interceptedMethods.containsKey(clz)) {
-            return interceptedMethods.get(clz);
-        } else {
-            List<Method> methods = extractInterceptedMethodsFromClassHierarchy(clz);
-            if (methods.size() <= 0) {
-                ErrorReporter.missingMeasurementConfig(clz);
-                throw new TimerException("no intercepted method defined for class: " + clz.getName());
-            }
-            interceptedMethods.put(clz, methods);
-            return methods;
-        }
-    }
 
     public List<MeasuredMethod> extract(Class<?> clz, Map<Class, List<Method>> interceptedMethods) throws NoSuchMethodException {
         List<Method> methods = new ArrayList<>();
@@ -48,9 +33,5 @@ public class MethodExtractor {
 
             recursiveExtract(clz.getSuperclass(), interceptedMethods, methods);
         }
-    }
-
-    private List<Method> extractInterceptedMethodsFromClassHierarchy(Class clz) {
-        return new ArrayList<>();
     }
 }
--- src/main/java/com/github/lkq/timeron/util/StringUtil.java
+++ src/main/java/com/github/lkq/timeron/util/StringUtil.java
@@ -1,25 +1,9 @@
 package com.github.lkq.timeron.util;
 
-import java.lang.reflect.Method;
-
 public class StringUtil {
 
     public static boolean isBlank(String value) {
         return value == null || "".equals(value.trim());
     }
 
-    public static String toString(Method method) {
-        StringBuilder keyBuilder = new StringBuilder(100)
-                .append(method.getDeclaringClass().getName())
-                .append(".")
-                .append(method.getName())
-                .append("(");
-        String comma = "";
-        for (Class<?> type : method.getParameterTypes()) {
-            keyBuilder.append(comma)
-                    .append(type.getSimpleName());
-        }
-        keyBuilder.append(")");
-        return keyBuilder.toString();
-    }
 }
